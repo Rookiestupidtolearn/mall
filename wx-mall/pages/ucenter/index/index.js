e7ac82c67e678e55ecebbfb923de9bed40f49fee
zhangran <1130879408@qq.com>
2018-10-16 14:47:50 +0800

    zr

--- wx-mall/pages/ucenter/index/index.js
+++ wx-mall/pages/ucenter/index/index.js
@@ -26,6 +26,7 @@ Page({
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
         console.log(app.globalData);
+        this.userAccount();
     },
     onReady: function () {
 
