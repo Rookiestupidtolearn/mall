e24e7d5ea9dbdb5fcdd18b1f6638b9bfd428dd9b
wuminglong <18401375498@163.com>
2018-10-19 16:55:04 +0800

    后台用户账户

--- platform-shop/src/main/webapp/js/shop/shopuser.js
+++ platform-shop/src/main/webapp/js/shop/shopuser.js
@@ -1,48 +1,3 @@
-$(function () {
-    $("#jqGrid").Grid({
-        url: '../user/list',
-        colModel: [{
-            label: 'id', name: 'id', index: 'id', key: true, hidden: true
-        }, {
-            label: '会员名称', name: 'username', index: 'username', width: 80
-        }, {
-            label: '会员密码', name: 'password', index: 'password', hidden: true
-        }, {
-            label: '性别', name: 'gender', index: 'gender', width: 40, formatter: function (value) {
-                return transGender(value);
-            }
-        }, {
-            label: '出生日期', name: 'birthday', index: 'birthday', width: 80, formatter: function (value) {
-                return transDate(value);
-            }
-        }, {
-            label: '注册时间', name: 'registerTime', index: 'register_time', width: 80, formatter: function (value) {
-                return transDate(value);
-            }
-        }, {
-            label: '最后登录时间', name: 'lastLoginTime', index: 'last_login_time', width: 80, formatter: function (value) {
-                return transDate(value);
-            }
-        }, {
-            label: '最后登录Ip', name: 'lastLoginIp', index: 'last_login_ip', hidden: true
-        }, {
-            label: '会员等级', name: 'levelName', width: 40
-        }, {
-            label: '微信名', name: 'nickname', index: 'nickname', width: 80
-        }, {
-            label: '手机号码', name: 'mobile', index: 'mobile', width: 120
-        }, {
-            label: '注册Ip', name: 'registerIp', index: 'register_ip', hidden: true
-        },{
-            label: '会员账户金额', name: 'amount', width: 40
-        }, {
-            label: '头像', name: 'avatar', index: 'avatar', width: 80, formatter: function (value) {
-                return transImg(value);
-            }
-        }, {
-            label: '微信Id', name: 'weixinOpenid', index: 'weixin_openid', width: 80, hidden: true
-        }]
-    });
 $(function() {
 	$("#jqGrid").Grid({
 		url : '../user/list',
@@ -118,7 +73,11 @@ $(function() {
 			name : 'registerIp',
 			index : 'register_ip',
 			hidden : true
-		}, {
+		},{
+            label: '会员账户金额',
+            name: 'amount',
+            width: 40
+        }, {
 			label : '头像',
 			name : 'avatar',
 			index : 'avatar',
@@ -170,6 +129,7 @@ var vm = new Vue({
 		recharge : function() {
 			var ids = $("#jqGrid").getGridParam("selarrrow");
 			var mobiles = [];
+			
 			var iday = [];
 			for (i = 0; i < ids.length; i++) {
 				var rowData = $("#jqGrid").jqGrid("getRowData", ids[i]);//根据上面的id获得本行的所有数据
@@ -184,7 +144,8 @@ var vm = new Vue({
 			vm.rechargeList = true;
 			vm.title = "充值";
 			vm.mobiles = mobiles.join(",");
-
+			 vm.amount = '';
+			 vm.memo = '';
 		},
 		rechargeSubmit : function() {
 			var url = "../qzrechargerecord/recharge";
