22ef42c679bc2cfc91bfb9367e1636a6ca9f054e
fengrongji <fengrongji@qzxq.com>
2018-10-24 18:08:56 +0800

    上传回写文件

--- platform-shop/src/main/java/com/platform/controller/QzUploadController.java
+++ platform-shop/src/main/java/com/platform/controller/QzUploadController.java
@@ -1,11 +1,19 @@
 package com.platform.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.platform.service.UploadService;
+import com.platform.utils.DateUtils;
+import com.platform.utils.ResourceUtil;
 
 @RestController
 @RequestMapping("qzUpload")
@@ -38,7 +48,56 @@ public class QzUploadController {
 		  error.add("请上传Excel文件");
 		     return error;  
       }
-	   List<String>  iStrings = uploadService.uploadRechargeExcel(file);
+	  
+	  String name = "";
+	  String path = "";
+       try {
+           InputStream fileInputStream = file.getInputStream();
+           //保存到服务器的路径
+          String homePath = ResourceUtil.getConfigByName("file.upload.excelPath");
+           String pathName = DateUtils.format(new Date(),"yyyyMMdd");
+            path = homePath+pathName+"/";
+   		  File fileNew =new File(path);    
+   		//如果文件夹不存在则创建    
+   		if  (!fileNew .exists()  && !fileNew .isDirectory())      
+   		{       
+   		    System.out.println("//文件夹不存在");  
+   		   fileNew .mkdirs();    
+   		}
+            name = DateUtils.format(new Date(),"yyyyMMddHHmmssSSS");
+           if(fileName.endsWith("xls")){
+               //2003
+        	   name +=".xls";
+           }else if(fileName.endsWith("xlsx")){
+               //2007 及2007以上
+        	   name +=".xlsx";
+           }
+           FileOutputStream fs=new FileOutputStream( path + File.separator + name);
+           byte[] buffer =new byte[1024*1024];
+           int bytesum = 0;
+           int byteread = 0;
+           while ((byteread=fileInputStream.read(buffer))!=-1)
+           {
+               bytesum+=byteread;
+               fs.write(buffer,0,byteread);
+               fs.flush();
+           }
+           fs.close();
+           fileInputStream.close();
+           
+           
+       } catch (IOException e) {
+           e.printStackTrace();
+       } 
+   
+      List<String> iStrings = new ArrayList<>();
+	try {
+		iStrings = uploadService.uploadRechargeExcelByHave(path,name);
+	} catch (IOException e) {
+	
+		e.printStackTrace();
+	}
+	   
 	  return iStrings ;
 	}
 }
