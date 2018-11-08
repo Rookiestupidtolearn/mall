a8ea8806b804f68ac3555e283bf3d5ac693ffa62
张春涛 <501195977@qq.com>
2018-11-08 13:15:42 +0800

    constant

---
+++ platform-api/src/main/java/com/platform/youle/constant/Constants.java
@@ -0,0 +1,13 @@
+package com.platform.youle.constant;
+
+/**
+ * 常量
+ */
+public class Constants {
+
+
+    private static String base_test_url = "http://beta.open.limofang.cn";
+
+    private static String base_prod_url = "http://beta.open.limofang.cn";
+
+}
