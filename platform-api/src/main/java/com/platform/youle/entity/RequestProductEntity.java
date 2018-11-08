7981933caab23ef2f83025bf0d93139d011cb909
fengrongji <fengrongji@qzxq.com>
2018-11-08 11:26:52 +0800

    获取产品

---
+++ platform-api/src/main/java/com/platform/youle/entity/RequestProductEntity.java
@@ -0,0 +1,16 @@
+package com.platform.youle.entity;
+
+public class RequestProductEntity extends RequestBaseEntity {
+   
+	private Integer page;
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+	
+	
+}
