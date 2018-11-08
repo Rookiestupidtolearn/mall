f0befcdcf4a672bc2c0d350fa825f17d4c69aefb
xusx <xusx@51lietou.com>
2018-11-08 14:43:42 +0800

    获取商品详情

--- platform-api/src/main/java/com/platform/youle/service/impl/ApiJdFuncServiceImpl.java
+++ platform-api/src/main/java/com/platform/youle/service/impl/ApiJdFuncServiceImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.platform.youle.constant.Constants;
 import com.platform.youle.entity.RequestSkuDetailEntity;
-import com.platform.youle.entity.ResponseBaseEntity;
 import com.platform.youle.entity.ResponseSkuDetailEntity;
 import com.platform.youle.service.ApiFuncService;
 import com.platform.youle.util.HttpUtil;
@@ -26,7 +26,7 @@ public class ApiJdFuncServiceImpl implements ApiFuncService {
 		String str = JSONObject.toJSONString(entity);//{"pid":1101,"timestamp":"1541654878029","token":"3B9576F327A4F5D252C0CEF7CE6CD55C","wid":"1322"}
 		logger.info("【获取单个商品详情请求参数】:" + str);
 		try {
-			String result = HttpUtil.post(skuDetailurl, str);
+			String result = HttpUtil.post(Constants.Urls.base_test_url.concat(Constants.Urls.detial), str);
 			logger.info("【获取单个商品详情响应参数】:" + result);
 			ResponseSkuDetailEntity reponse = JSONObject.parseObject(result,new TypeReference<ResponseSkuDetailEntity>(){});
 			return reponse;
