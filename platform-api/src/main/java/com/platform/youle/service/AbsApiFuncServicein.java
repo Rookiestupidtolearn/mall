75f2bda0f658a659f02a54c26f6ee55f2ab0319e
张春涛 <501195977@qq.com>
2018-11-08 15:59:24 +0800

    代码调整

--- platform-api/src/main/java/com/platform/youle/service/AbsApiFuncServicein.java
+++ platform-api/src/main/java/com/platform/youle/service/AbsApiFuncServicein.java
@@ -1,6 +1,8 @@
 package com.platform.youle.service;
 
+import com.alibaba.fastjson.JSON;
 import com.platform.youle.entity.*;
+import com.platform.youle.util.HttpUtil;
 import com.platform.youle.util.TokenUtil;
 
 import java.util.Calendar;
@@ -11,7 +13,9 @@ import java.util.Map;
  */
 public abstract  class AbsApiFuncServicein implements IApiFuncServicein{
 
+
     /**
+     *
      * 初始请求参数
      * @param entity
      */
@@ -20,6 +24,20 @@ public abstract  class AbsApiFuncServicein implements IApiFuncServicein{
         entity.setTimestamp(String.valueOf(Calendar.getInstance().getTimeInMillis()));
         entity.setToken(TokenUtil.token);
     }
+
+    /**
+     * 实体转map
+     * @param entity
+     * @return
+     * @throws Exception
+     */
+    public Map<String,Object>  objectToMap(RequestBaseEntity entity) throws Exception{
+            String str = JSON.toJSONString(entity);
+            Map<String,Object> map = (Map<String,Object>) JSON.parse(str);
+            return map;
+    }
+
+
 
     /**
      * 1.1获取所有商品ID
