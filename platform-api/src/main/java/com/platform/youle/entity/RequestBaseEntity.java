ddbfbc842b7f0c70ed7cca1661efa27bea9a2945
fengrongji <fengrongji@qzxq.com>
2018-11-08 11:21:25 +0800

    实体

---
+++ platform-api/src/main/java/com/platform/youle/entity/RequestBaseEntity.java
@@ -0,0 +1,29 @@
+package com.platform.youle.entity;
+
+public class RequestBaseEntity {
+  
+	public String  wid;
+	public String  token;
+	public String timestamp;
+	public String getWid() {
+		return wid;
+	}
+	public void setWid(String wid) {
+		this.wid = wid;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
+
+}
