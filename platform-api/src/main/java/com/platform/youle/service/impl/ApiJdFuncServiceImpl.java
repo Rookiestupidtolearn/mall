19dae415b3a7367346ed8a863287e807cb328d76
xusx <xusx@51lietou.com>
2018-11-08 17:11:57 +0800

    1.3 获取订单详情接口

--- platform-api/src/main/java/com/platform/youle/service/impl/ApiJdFuncServiceImpl.java
+++ platform-api/src/main/java/com/platform/youle/service/impl/ApiJdFuncServiceImpl.java
@@ -3,12 +3,12 @@ package com.platform.youle.service.impl;
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Calendar;
 import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.druid.util.StringUtils;
@@ -16,6 +16,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.platform.dao.JdGoodsImgPathMapper;
+import com.platform.dao.JdGoodsMapper;
 import com.platform.youle.constant.Constants;
 import com.platform.youle.constant.Constants.Urls;
 import com.platform.youle.entity.GoodsImagePathVo;
@@ -24,7 +26,6 @@ import com.platform.youle.entity.RequestBaseEntity;
 import com.platform.youle.entity.RequestProductEntity;
 import com.platform.youle.entity.ResponseBaseEntity;
 import com.platform.youle.entity.ResponseProductEntity;
-import com.platform.youle.entity.ResponseSkuDetailEntity;
 import com.platform.youle.entity.ResponseSaleStatusEntity;
 import com.platform.youle.entity.ResponseSkuDetailEntity;
 import com.platform.youle.service.AbsApiFuncServicein;
@@ -35,25 +36,27 @@ import com.platform.youle.util.TokenUtil;
 public class ApiJdFuncServiceImpl extends AbsApiFuncServicein {
 
     private static final Logger logger = LoggerFactory.getLogger(ApiJdFuncServiceImpl.class);
+    @Autowired
+    private JdGoodsMapper jdGoodsMapper;
+    @Autowired
+    private JdGoodsImgPathMapper jdGoodsImgPathMapper;
 
 	@Override
 	public ResponseSkuDetailEntity getSkuDetail(Map<String,Object> params) {
-		logger.info("【获取单个商品详情请求参数】:" + params);
+		
+		logger.info("[1.3获取单个商品详情]入参：" + params);
 		try {
 			String result = HttpUtil.post(Constants.Urls.base_test_url.concat(Constants.Urls.detial), params);
-			logger.info("【获取单个商品详情响应参数】:" + result);
+			logger.info("[1.3获取单个商品详情]响应参数：" + result);
 			ResponseSkuDetailEntity reponse = JSONObject.parseObject(result,new TypeReference<ResponseSkuDetailEntity>(){});
 			if(reponse != null){
 				if(reponse.isRESPONSE_STATUS()){
-					JdGoodsVo good = new JdGoodsVo();
-					GoodsImagePathVo imagePath = new GoodsImagePathVo();
-					
-					
-					
+					saveGoodInfo(reponse.getRESULT_DATA(),Long.parseLong(params.get("pid").toString()));
 				}
 			}
 			return reponse;
 		} catch (Exception e) {
+			logger.info("[1.3获取单个商品详情]异常：", e);
 			e.printStackTrace();
 		}
 		return null;
@@ -74,95 +77,70 @@ public class ApiJdFuncServiceImpl extends AbsApiFuncServicein {
 		}
 		return reponse;
 	}
-
-	public static void main(String[] args) {
-		String str = " { "
-				 +" 	'RESPONSE_STATUS': 'true', "
-				 +" 	'RESULT_DATA': { "
-				 +" 		'PRODUCT_DATA': { "
-				 +" 			'productId': 1101, "
-				 +" 			'name': 'API接口测试产品', "
-				 +" 			'type': 'system', "
-				 +" 			'thumbnailImage': 'http://img.fygift.com/thumbnail/2016/6/4447263056278594047.jpg', "
-				 +" 			'brand': '九阳', "
-				 +" 			'productCate': 896, "
-				 +" 			'productCode': 'GALAXY29', "
-				 +" 			'status': 'selling', "
-				 +" 			'marketPrice': 2427.0, "
-				 +" 			'retailPrice': 29.0, "
-				 +" 			'productPlace': '深圳', "
-				 +" 			'features': '这是一个测试产品, 勿拍', "
-				 +" 			'hot': false, "
-				 +" 			'createTime': '2016-03-18 13:41:28', "
-				 +" 			'is7ToReturn': false "
-				 +" 		}, "
-				 +" 		'PRODUCT_IMAGE': [{ "
-				 +" 				'imageUrl': 'http://img.fygift.com//2016/6/4447263056278594047.jpg', "
-				 +" 				'orderSort': 0 "
-				 +" 			}, "
-				 +" 			{ "
-				 +" 				'imageUrl': 'http://img.fygift.com//2016/6/845661839931297772.jpg', "
-				 +" 				'orderSort': 1 "
-				 +" 			} "
-				 +" 		], "
-				 +" 		'PRODUCT_DESCRIPTION': '产品名称：空气净化器</br >产品型号：RSD-JC66R</br>外箱尺寸 540*278*680mm <p > < img src = ’http: //img.fygift.com/attach/2014/10/8624759654857092274.jpg’ style=’height:634px; width:725px’/></p> ', "
-				 +" 		'MOBILE_PRODUCT_DESCRIPTION': '' "
-				 +" 	} "
-				 +" } ";
-		JSONObject dateObj = JSONObject.parseObject(str);
-		String resultObj = dateObj.get("RESULT_DATA").toString();
-		if(!StringUtils.isEmpty(resultObj)){
-			JSONObject resultDate = JSONObject.parseObject(resultObj);
-			JdGoodsVo good = new JdGoodsVo();
-			
-			if(resultDate.getString("PRODUCT_IMAGE") != null){
-				JSONArray imgArrays = JSONArray.parseArray(resultDate.getString("PRODUCT_IMAGE").toString());
-				if(!CollectionUtils.isEmpty(imgArrays)){
-					for(int i = 0;i<imgArrays.size();i++){
-						JSONObject obj = JSONObject.parseObject(imgArrays.get(i).toString());
-						GoodsImagePathVo imagePath = new GoodsImagePathVo();
-						imagePath.setImageUrl(obj.get("imageUrl").toString());
-						imagePath.setOrderSort(Integer.parseInt(obj.get("orderSort").toString()));
-						imagePath.setCreateTime(new Date());
-						imagePath.setUpdateTime(new Date());
+	
+	public JSONObject saveGoodInfo(String response,Long productId){
+		JSONObject resObj = new JSONObject();
+		try {
+			JSONObject dateObj = JSONObject.parseObject(response);
+			String resultObj = dateObj.get("RESULT_DATA").toString();
+			if(!StringUtils.isEmpty(resultObj)){
+				JSONObject resultDate = JSONObject.parseObject(resultObj);
+				JdGoodsVo good = new JdGoodsVo();
+				
+				if(resultDate.getString("PRODUCT_IMAGE") != null){
+					JSONArray imgArrays = JSONArray.parseArray(resultDate.getString("PRODUCT_IMAGE").toString());
+					if(!CollectionUtils.isEmpty(imgArrays)){
+						for(int i = 0;i<imgArrays.size();i++){
+							JSONObject obj = JSONObject.parseObject(imgArrays.get(i).toString());
+							GoodsImagePathVo imagePath = new GoodsImagePathVo();
+							imagePath.setImageUrl(obj.get("imageUrl").toString());
+							imagePath.setOrderSort(Integer.parseInt(obj.get("orderSort").toString()));
+							imagePath.setCreateTime(new Date());
+							imagePath.setUpdateTime(new Date());
+							imagePath.setGoodsSn(productId);
+							jdGoodsImgPathMapper.save(imagePath);
+						}
 					}
 				}
-			}
-			
-			
-			
-			good.setMobileProductDecription(resultDate.get("MOBILE_PRODUCT_DESCRIPTION") == null ? "" : resultDate.get("MOBILE_PRODUCT_DESCRIPTION").toString());
-			good.setProductDecription(resultDate.get("PRODUCT_DESCRIPTION") == null ? "" : resultDate.get("PRODUCT_DESCRIPTION").toString());
-			
-			if(resultDate.getString("PRODUCT_DATA") != null){
-				String productDate = resultDate.getString("PRODUCT_DATA").toString();
-				JSONObject productObj = JSONObject.parseObject(productDate);
-				good.setGoodsSn(productObj.get("productId") == null ? null : Long.parseLong(productObj.get("productId").toString()));
-				good.setName(productObj.get("name") == null ? "" : productObj.get("name").toString());
-				good.setType(productObj.get("type") == null ? "" : productObj.get("type").toString());
-				good.setThumbnailImage(productObj.get("thumbnailImage") == null ? "" : productObj.get("thumbnailImage").toString());
-				good.setBrand(productObj.get("brand") == null ? "" : productObj.get("brand").toString());
-				good.setProductCate(productObj.get("productCate") == null ? null : Integer.parseInt(productObj.get("productCate").toString()));
-				good.setProductCode(productObj.get("productCode") == null ? "" : productObj.get("productCode").toString());
-				good.setStatus(productObj.get("status") == null ? "" : productObj.get("status").toString());
-				good.setMarketPrice(productObj.get("marketPrice") == null ? null : new BigDecimal(productObj.get("marketPrice").toString()));
-				good.setRetailPrice(productObj.get("retailPrice") == null ? null : new BigDecimal(productObj.get("retailPrice").toString()));
-				good.setProductPlace(productObj.get("productPlace") == null ? "" : productObj.get("productPlace").toString());
-				good.setFeatures(productObj.get("features") == null ? "" : productObj.get("features").toString());
-				if(productObj.get("hot") != null){
-					good.setHot("true".equals(productObj.get("hot").toString()) ? 1: 0);
-					
+				
+				good.setMobileProductDecription(resultDate.get("MOBILE_PRODUCT_DESCRIPTION") == null ? "" : resultDate.get("MOBILE_PRODUCT_DESCRIPTION").toString());
+				good.setProductDecription(resultDate.get("PRODUCT_DESCRIPTION") == null ? "" : resultDate.get("PRODUCT_DESCRIPTION").toString());
+				
+				if(resultDate.getString("PRODUCT_DATA") != null){
+					String productDate = resultDate.getString("PRODUCT_DATA").toString();
+					JSONObject productObj = JSONObject.parseObject(productDate);
+					good.setGoodsSn(productObj.get("productId") == null ? null : Long.parseLong(productObj.get("productId").toString()));
+					good.setName(productObj.get("name") == null ? "" : productObj.get("name").toString());
+					good.setType(productObj.get("type") == null ? "" : productObj.get("type").toString());
+					good.setThumbnailImage(productObj.get("thumbnailImage") == null ? "" : productObj.get("thumbnailImage").toString());
+					good.setBrand(productObj.get("brand") == null ? "" : productObj.get("brand").toString());
+					good.setProductCate(productObj.get("productCate") == null ? null : Integer.parseInt(productObj.get("productCate").toString()));
+					good.setProductCode(productObj.get("productCode") == null ? "" : productObj.get("productCode").toString());
+					good.setStatus(productObj.get("status") == null ? "" : productObj.get("status").toString());
+					good.setMarketPrice(productObj.get("marketPrice") == null ? null : new BigDecimal(productObj.get("marketPrice").toString()));
+					good.setRetailPrice(productObj.get("retailPrice") == null ? null : new BigDecimal(productObj.get("retailPrice").toString()));
+					good.setProductPlace(productObj.get("productPlace") == null ? "" : productObj.get("productPlace").toString());
+					good.setFeatures(productObj.get("features") == null ? "" : productObj.get("features").toString());
+					if(productObj.get("hot") != null){
+						good.setHot("true".equals(productObj.get("hot").toString()) ? 1: 0);
+					}
+					if(productObj.get("is7ToReturn") != null){
+						good.setIs7Toreturn("true".equals(productObj.get("is7ToReturn").toString()) ? 1: 0);
+					}
+					good.setCreateTime(new Date());
+					good.setId(productId);
 				}
-				if(productObj.get("is7ToReturn") != null){
-					good.setIs7Toreturn("true".equals(productObj.get("is7ToReturn").toString()) ? 1: 0);
-					
-				}
-				good.setCreateTime(new Date());
+				jdGoodsMapper.save(good);
 			}
-			
+			resObj.put("state", "success");
+		} catch (Exception e) {
+			resObj.put("state", "fail");
+			logger.info("[1.3获取单个商品详情,商品入库]异常：", e);
 		}
-
+		return resObj;
 	}
+	
+	
 	@Override
 	public ResponseProductEntity getProductIdsByPage(Integer page) {
 		
