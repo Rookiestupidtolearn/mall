ed7b620f85fbc926a3d07a87203e17395abb1c97
wuminglong <18401375498@163.com>
2018-11-15 19:59:43 +0800

    计算支付配比

--- platform-api/src/main/java/com/platform/util/PayMatchingUtil.java
+++ platform-api/src/main/java/com/platform/util/PayMatchingUtil.java
@@ -3,8 +3,10 @@ package com.platform.util;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.platform.dao.ApiProductMapper;
 import com.platform.entity.ProductVo;
@@ -12,25 +14,21 @@ import com.platform.utils.SpringContextUtils;
 
 
 /**
- * 计算支付配比工具类
+ * 计算支付配比
  *
  */
-public class PayMatchingUtil implements InitializingBean {
+@Component
+public class PayMatchingUtil {
 
 	 @Autowired
-	 private static ApiProductMapper apiProductMapper;
-	 
-	 public static void init() {
-		if(null == apiProductMapper){
-			apiProductMapper = SpringContextUtils.getBean(ApiProductMapper.class);
-		}
-	 }
+	 public ApiProductMapper apiProductMapper;
+	
 	/**
 	 * 获取商品支付配比
 	 * @param goodsId
 	 * @return
 	 */
-	public static Map<Object,Object> getPayMatching(Integer productId){
+	public  Map<Object,Object> getPayMatching(Integer productId){
 		Map<Object,Object> goodsPayMatching = new HashMap<>();
 		if(null == productId || 0 >= productId){
 			return goodsPayMatching;
@@ -44,16 +42,12 @@ public class PayMatchingUtil implements InitializingBean {
 		BigDecimal retailPrice = productVo.getRetail_price(); // 结算价 N
 		BigDecimal activityMatching = new BigDecimal(productVo.getActivity_matching()); //活动配比 H
 		BigDecimal normalMatching =  new BigDecimal(productVo.getNormal_matching()); //正常配比 Z
-		//计算支付配比 : 公式 S={M*Z-(Z-H)*N}/M*100%；
+		
+		//计算支付配比 : 公式 S={M*Z-(Z-H)*N}/M*100%
 		BigDecimal PayMatching = marketPrice.multiply(normalMatching).subtract(normalMatching.subtract(activityMatching).multiply(retailPrice)).divide(marketPrice);
 		//计算抵扣金额
-		BigDecimal Money = marketPrice.multiply(PayMatching);
+		BigDecimal Money = marketPrice.multiply(PayMatching).setScale(2, BigDecimal.ROUND_HALF_UP);
 		goodsPayMatching.put(productVo.getGoods_id(), Money);
 		return goodsPayMatching;
-	}
-	
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		init();
 	}
 }
