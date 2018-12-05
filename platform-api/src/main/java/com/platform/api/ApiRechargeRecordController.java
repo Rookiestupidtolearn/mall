d85845dcb7e5ae9aef09124ea04f14c7925050fe
fengrongji <fengrongji@qzxq.com>
2018-12-05 14:12:02 +0800

    初始化

---
+++ platform-api/src/main/java/com/platform/api/ApiRechargeRecordController.java
@@ -0,0 +1,29 @@
+package com.platform.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.platform.annotation.IgnoreAuth;
+import com.platform.util.ApiBaseAction;
+import com.sun.xml.internal.xsom.impl.scd.Iterators.Map;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "充值记录")
+@RestController
+@RequestMapping("/api/rechargeRecord")
+public class ApiRechargeRecordController  extends ApiBaseAction  {
+
+	
+//	
+//	@ApiOperation(value ="充值提交", response = Map.class)
+//	@PostMapping("subbmitRecharge")
+//	@IgnoreAuth
+//	public Object   rechargeRecordSubbmit(){
+//		
+//		
+//	}
+	
+}
