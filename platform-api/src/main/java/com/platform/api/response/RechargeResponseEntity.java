80dbcdee27b0f0d81f8cf210ba0ab8e9d190ec4f
fengrongji <fengrongji@qizidai.com>
2018-12-18 14:19:41 +0800

    测试充值

--- platform-api/src/main/java/com/platform/api/response/RechargeResponseEntity.java
+++ platform-api/src/main/java/com/platform/api/response/RechargeResponseEntity.java
@@ -3,9 +3,13 @@ package com.platform.api.response;
 public class RechargeResponseEntity {
 
 	/**
-	 * 200 充值成功
-	 * error
+	 * success 充值成功
+	 * error  充值失败 
 	 * 1000 解析密文失败
+	 * 1001 手机号错误 
+	 * 1002 金额错误  10万
+	 * 1003 订单编号错误 ,最长50位
+	 * 1004 充值渠错误 ,2-奇速贷，3-其他
 	 */
 	private String code;
 	private String msg;
