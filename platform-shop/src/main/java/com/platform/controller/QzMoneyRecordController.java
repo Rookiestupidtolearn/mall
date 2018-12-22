f6383314a950265cd05dc0bbae41172aa7e12f6e
xuxuxu <xushaoxiong@qizidai.com>
2018-12-22 11:22:35 +0800

    导出

--- platform-shop/src/main/java/com/platform/controller/QzMoneyRecordController.java
+++ platform-shop/src/main/java/com/platform/controller/QzMoneyRecordController.java
@@ -1,7 +1,11 @@
 package com.platform.controller;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.platform.entity.QzMoneyRecordEntity;
+import com.platform.entity.UserEntity;
 import com.platform.service.QzMoneyRecordService;
 import com.platform.utils.PageUtils;
 import com.platform.utils.Query;
 import com.platform.utils.R;
+import com.platform.utils.excel.ExcelExport;
 
 /**
  * 用户资金流水Controller
@@ -100,5 +106,46 @@ public class QzMoneyRecordController {
         List<QzMoneyRecordEntity> list = qzMoneyRecordService.queryList(params);
 
         return R.ok().put("list", list);
+    }
+
+    /**
+     * 导出会员
+     */
+    @RequestMapping("/export")
+    @RequiresPermissions("qzmoneyrecord:export")
+    public R export(@RequestParam Map<String, Object> params, HttpServletResponse response) {
+
+    	  //查询列表数据
+    	System.out.println("111");
+        Query query = new Query(params);
+
+        List<QzMoneyRecordEntity> qzMoneyRecordList = qzMoneyRecordService.queryList(query);
+
+        ExcelExport ee = new ExcelExport("平台流水列表");
+
+        String[] header = new String[]{"会员名称", "会员id","手机号","身份证件号","资金变动类型","金额变动标志","变动金额","冻结金额","当前余额","创建时间","交易流水号"};
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        if (qzMoneyRecordList != null && qzMoneyRecordList.size() != 0) {
+            for (QzMoneyRecordEntity record : qzMoneyRecordList) {
+                LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+                map.put("USERNAME", record.getShopUserName());
+                map.put("USERID", record.getShopUserId());
+                map.put("PHONE", record.getUserPhone());
+                map.put("IDCARD", record.getCardId());
+                map.put("TRANtYPE", "1".equals(record.getTranType()) ? "平台充值" : "消费");
+                map.put("TRANFLAG", record.getTranFlag() == 0 ? "支出" :"收入");
+                map.put("TRANAMOUNT", record.getTarnAmount());
+                map.put("LOCKAMOUNT", record.getLockAmount());
+                map.put("CURRENTAMOUNT", record.getCurrentAmount());
+                map.put("CREATEDATE", record.getCreateTime());
+                map.put("TRANNO", record.getTradeNo());
+                list.add(map);
+            }
+        }
+        ee.addSheetByMap("流水", list, header);
+        ee.export(response);
+        return R.ok();
     }
 }
