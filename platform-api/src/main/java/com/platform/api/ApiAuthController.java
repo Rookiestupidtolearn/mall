a7335b3de354bb2cb1c1fa812ab22eb5baa221f4
fengrongji <fengrongji@qizidai.com>
2018-12-25 15:38:25 +0800

    去掉验证码

--- platform-api/src/main/java/com/platform/api/ApiAuthController.java
+++ platform-api/src/main/java/com/platform/api/ApiAuthController.java
@@ -189,22 +189,22 @@ public class ApiAuthController extends ApiBaseAction {
 		
 		Level2Cache level2 = CacheProviderHolder.getLevel2Cache(J2CacheUtils.INVALID_CACHE);
 		Integer count = (Integer) level2.get("DOUBAO_SMS_COUNT:" + mobile);
-		if(count !=null &&count>=6){
-			String  checkcode = params.get("yzm");
-			if(StringUtils.isNullOrEmpty(checkcode)){
-			  	 return R.error("图形验证码不能为空！");
-			}
-			 String imageCode = (String) requset.getSession().getAttribute("imageCode");
-			if (org.apache.commons.lang.StringUtils.isEmpty(imageCode)) {
-				return R.error("图形验证码失效！");
-			}
-			
-			if (!checkcode.equals(imageCode)) {
-				return R.error("图形验证码不正确！");
-			}
-			requset.removeAttribute("imageCode");
-		  
-		}
+//		if(count !=null &&count>=6){
+//			String  checkcode = params.get("yzm");
+//			if(StringUtils.isNullOrEmpty(checkcode)){
+//			  	 return R.error("图形验证码不能为空！");
+//			}
+//			 String imageCode = (String) requset.getSession().getAttribute("imageCode");
+//			if (org.apache.commons.lang.StringUtils.isEmpty(imageCode)) {
+//				return R.error("图形验证码失效！");
+//			}
+//			
+//			if (!checkcode.equals(imageCode)) {
+//				return R.error("图形验证码不正确！");
+//			}
+//			requset.removeAttribute("imageCode");
+//		  
+//		}
 	
 		UserVo userVo = userService.queryByMobile(mobile);
        if (userVo== null) {
