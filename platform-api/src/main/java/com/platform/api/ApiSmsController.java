f8954747d77ae4b588afbd3323493d89c5797c24
fengrongji <fengrongji@qizidai.com>
2018-12-29 17:16:12 +0800

    验证每

--- platform-api/src/main/java/com/platform/api/ApiSmsController.java
+++ platform-api/src/main/java/com/platform/api/ApiSmsController.java
@@ -71,16 +71,13 @@ public class ApiSmsController {
     @IgnoreAuth
     public void captcha(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
     	
-        response.setHeader("Cache-Control", "no-store, no-cache");
+   //     response.setHeader("Cache-Control", "no-store, no-cache");
         response.setContentType("image/jpeg");
         //生成文字验证码
         String text = producer.createText();
         //生成图片验证码
         BufferedImage image = producer.createImage(text);
-       // request.setAttribute(DefaultSubjectContext.SESSION_CREATION_ENABLED, Boolean.TRUE);
         ShiroUtils.setSessionAttribute(Constants.KAPTCHA_SESSION_KEY, text);
-//       HttpSession session = request.getSession();
-//       session.setAttribute("imageCode", text);
         ServletOutputStream out = response.getOutputStream();
         ImageIO.write(image, "jpg", out);
     }
