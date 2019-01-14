7cecbdf3c0a582012854733f2a36272400419604
zhangran <1130879408@qq.com>
2019-01-14 10:22:42 +0800

    订单操作功能展示优化

--- platform-shop/src/main/webapp/WEB-INF/doubao/src/pages/ucenter/orderDetail.vue
+++ platform-shop/src/main/webapp/WEB-INF/doubao/src/pages/ucenter/orderDetail.vue
@@ -11,8 +11,14 @@
                 <div v-if="orderInfo.order_status == 9" >
                 	<a href="javascript:;"  class="zhiCustomBtn btn active" id="btn" @click="ccHref">退货申请</a>
                 </div>
-                 <div v-else-if="orderInfo.order_status == 300 || orderInfo.order_status == 201 || orderInfo.order_status == 200">
+                 <div v-else-if="orderInfo.order_status == 201 || orderInfo.order_status == 200">
                  	<div class="btn" @click="hrefwul(orderInfo.id)">查看物流</div>
+                 	<a href="javascript:;"  class="zhiCustomBtn btn active" id="btn" @click="ccHref">取消订单</a>
+                	<div class="btn active" @click="confirmOrder(orderInfo.id)">确认收货</div>
+                </div>
+                 <div v-else-if="orderInfo.order_status == 300">
+                 	<div class="btn" @click="hrefwul(orderInfo.id)">查看物流</div>
+                 	<a href="javascript:;"  class="zhiCustomBtn btn active" id="btn" @click="ccHref">退货申请</a>
                 	<div class="btn active" @click="confirmOrder(orderInfo.id)">确认收货</div>
                 </div>
                 <div v-else-if="orderInfo.order_status == 0">   
