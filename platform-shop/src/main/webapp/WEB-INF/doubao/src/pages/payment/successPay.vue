28a00a301089830f61e97e7c4b5d679c42441443
zhangran <1130879408@qq.com>
2019-01-18 13:40:12 +0800

    支付完成后联调

--- platform-shop/src/main/webapp/WEB-INF/doubao/src/pages/payment/successPay.vue
+++ platform-shop/src/main/webapp/WEB-INF/doubao/src/pages/payment/successPay.vue
@@ -25,12 +25,39 @@ export default {
 	 },
 	 methods:{
 	 	look(){
-	 		this.$router.push('/pages/ucenter/order')
+	 		var appHref = window.location.href;
+			var comHref = window.location.origin;
+			if(appHref.indexOf('device')>-1){
+				var device = appHref.substring(appHref.indexOf('device')).split('=')[1].split('&')[0];
+			}
+	    	if(device == 'android'){
+	    			window.android.viewOrder(comHref +'/#/pages/ucenter/order'); //调起andriod交互方法(由app发起。浏览器会报错正常)
+	    			return false;
+	    	}else if(device == 'ios'){
+	    			var message = {'url':comHref +'/#/pages/ucenter/order'}
+						window.webkit.messageHandlers.webViewApp.postMessage(message);
+						return false;
+	    	}else{
+	    		this.$router.push('/pages/ucenter/order');
+	    	}
 	 	},
 	 	success(){
-	 		this.$router.push('/pages/shoppingcar')
+	 		var appHref = window.location.href;
+			var comHref = window.location.origin;
+			if(appHref.indexOf('device')>-1){
+				var device = appHref.substring(appHref.indexOf('device')).split('=')[1].split('&')[0];
+			}
+	    	if(device == 'android'){
+	    			window.android.payFinish(); //调起andriod交互方法(由app发起。浏览器会报错正常)
+	    			return false;
+	    	}else if(device == 'ios'){
+	    			var message = {'url':'payFinish'}
+						window.webkit.messageHandlers.webViewApp.postMessage(message);
+						return false;
+	    	}else{
+	    		this.$router.push('/pages/shoppingcar');
+	    	}
 	 	}
-	 	
  	}
 }
 </script>
