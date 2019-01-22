8a5f625a766aada94ff81863a8d7081c4c4e5ac8
zhangran <1130879408@qq.com>
2019-01-22 15:41:40 +0800

    订单与原生做交互

--- platform-shop/src/main/webapp/WEB-INF/doubao/src/components/showTan.vue
+++ platform-shop/src/main/webapp/WEB-INF/doubao/src/components/showTan.vue
@@ -38,8 +38,21 @@
 	   	unsells:''
 	   },
 	   methods:{
-	   	cancelShop(){
-	   		this.$router.push('/pages/shoppingcar');
+	   	cancelShop(){    
+	   		var appHref = window.location.href;
+			if(appHref.indexOf('device')>-1){
+				var device = appHref.substring(appHref.indexOf('device')).split('=')[1].split('&')[0];
+			}
+	   		if(device == 'android'){
+	    			window.android.toShopCart(); //调起andriod交互方法(由app发起。浏览器会报错正常)
+	    			return false;
+	    	}else if(device == 'ios'){
+	    			var message = {'url':'toShopCart'}
+					window.webkit.messageHandlers.webViewApp.postMessage(message);
+					return false;
+	    	}else{
+	    		this.$router.push('/pages/shoppingcar');
+	    	}
 	   	}
 	   }
 	}
